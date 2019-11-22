456b7082fa0a4c0233c61e452b5b3a296a595bd0
Brandon Trabucco <brandon@btrabucco.com>
2019-11-22 14:26:01 -0800

    updated imports

--- controls/__init__.py
+++ controls/__init__.py
@@ -1,6 +1,7 @@
 """Author: Brandon Trabucco, Copyright 2019, MIT License"""
 
 
-from controls.lqr.lqr import lqr
 from controls.shooting.shooting import shooting
+from controls.lqr.lqr import lqr
+from controls.cem import cem
 from controls.iterative_lqr import iterative_lqr
