e4ef9243a65a0dfcf5bc8416a0248de75b4bc8f1
Brandon Trabucco <brandon@btrabucco.com>
2019-12-03 22:13:35 -0800

    updated distributions

--- controls/__init__.py
+++ controls/__init__.py
@@ -1,7 +1,13 @@
 """Author: Brandon Trabucco, Copyright 2019, MIT License"""
 
 
-from controls.lqr.lqr import lqr
 from controls.cem import cem
 from controls.shooting import shooting
 from controls.iterative_lqr import iterative_lqr
+from controls.lqr.lqr import lqr
+from controls.distributions.gaussian import Gaussian
+from controls.distributions.tanh_gaussian import TanhGaussian
+from controls.distributions.deterministic import Deterministic
+from controls.distributions.categorical import Categorical
+from controls.distributions import UnitGaussian
+from controls.distributions import UniformCategorical
--- controls/distributions/__init__.py
+++ controls/distributions/__init__.py
@@ -1,1 +1,39 @@
 """Author: Brandon Trabucco, Copyright 2019, MIT License"""
+
+
+from controls.distributions.gaussian import Gaussian
+from controls.distributions.tanh_gaussian import TanhGaussian
+from controls.distributions.deterministic import Deterministic
+from controls.distributions.categorical import Categorical
+import tensorflow as tf
+
+
+class UnitGaussian(Gaussian):
+
+    def __init__(self, size=1):
+        """Create a distribution for a unit gaussian variable.
+
+        Args:
+        - size: an integer, the size of the vector space.
+        """
+        Gaussian.__init__(
+            self,
+            lambda time, inputs: (
+                tf.zeros([tf.shape(inputs[0])[0], size]),
+                tf.ones([tf.shape(inputs[0])[0], size, size]),
+                tf.ones([tf.shape(inputs[0])[0], size, size]),
+                tf.zeros([tf.shape(inputs[0])[0]]),))
+
+
+class UniformCategorical(Categorical):
+
+    def __init__(self, size=1):
+        """Create a distribution for a unit gaussian variable.
+
+        Args:
+        - size: the number of categorical options.
+        """
+        Categorical.__init__(
+            self,
+            lambda time, inputs: (
+                tf.zeros([tf.shape(inputs[0])[0], size]),))
